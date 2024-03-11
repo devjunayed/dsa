33225cc68fc92179659d3c1ed5c2886464dd8e09
Md Junayed <devjunayed@gmail.com>
2024-03-11 22:43:49 +0600

    array basics done

---
+++ Section_2_Essential_C_and_C++_ concepts/c++/3_array_basics.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5] = {2, 3, 4, 5, 6};
+    int i;
+
+    for (i = 0; i < 5; i++)
+    {
+        cout << arr[i] << endl;
+    }
+    return 0;
+}
---
+++ Section_2_Essential_C_and_C++_ concepts/js/3_array_basics.js
@@ -0,0 +1,5 @@
+var arr = [2, 4, 5, 6, 7];
+
+for(var i = 0; i < 5; i++ ){
+     console.log(arr[i]);
+}
