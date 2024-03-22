bc9abedd6555663671251862eefc7b685a5c8889
Md Junayed <devjunayed@gmail.com>
2024-03-22 22:46:21 +0600

    pointers and reference done

--- Section_2_Essential_C_and_C++_ concepts/6_structures.cpp
+++ Section_2_Essential_C_and_C++_ concepts/6_structures.cpp
@@ -1,21 +1,35 @@
 #include <iostream>
 using namespace std;
 
-struct Rectangle
-{
-    int length;
-    int breadth;
-    //this will take 4 byte of memory instead of 1byte which is called padding of memory
-    char x; 
+struct Rectangle{
+    int length; // 4 byte
+    int breadth; // 4 byte
 
+    char x; // 1 byte for char but it will take 4 byte which will be known as padding
 };
+
+// also can decalre with definition lik this
+// struct Rectangle{
+//     int length;
+//     int breadth;
+// } r1, r2;
+
+// also like this we can declarre
+// struct Reactangle r1, r2, r3;
+
 
 int main()
 {
-    struct Rectangle r = {10, 5};
+    struct Rectangle r1 = {10, 5}; // value initailization
 
-    cout << r.length * r.breadth << endl;
+    // reassigning values
+    r1.length = 15;
+    r1.breadth = 7;
 
-    cout << sizeof(r) << endl;
+    cout << sizeof(r1) << endl; // will output 8 byte
+    cout << r1.length << endl;
+    cout << r1.breadth << endl;
+
+    cout << r1.length * r1.breadth << endl;
     return 0;
 }
