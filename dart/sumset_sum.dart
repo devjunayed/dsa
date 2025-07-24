5ac266313ece28e83bf0c19ec74b20490bff03ba
devjunayed <devjunayed@gmail.com>
2025-07-24 17:31:04 +0600

    pattern one is completed

---
+++ dart/sumset_sum.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+main() {
+  List<String> line1 = stdin.readLineSync()!.split(' ');
+  List<String> line2 = stdin.readLineSync()!.split(' ');
+
+  int n = int.parse(line1[0]);
+  int x = int.parse(line1[1]);
+
+  if (line2.length == n) {
+      
+  }
+}
---
+++ strivers/step_1_Learn_the_basics/Lec_2_Build_up_logical_thinking/pattern_1.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
+            cout << "* " ;
+        }
+        cout << endl;
+    }
+    return 0;
+}
